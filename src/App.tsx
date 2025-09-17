ea4a0356696aa544b4526fc2501c228e9ffef055
Mariane <mariane.azevedo@estudante.ifms.edu.br>
2025-09-17 12:52:56 -0300

    criação do post

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,42 @@ type ProdutoType = {
 
 function App() {
   const [produtos, setProdutos] = useState<ProdutoType[]>([])
+  
   useEffect(() => {
     fetch('/api/produtos')
       .then((response) => response.json())
       .then((data) => setProdutos(data))
+      .catch((error) => console.error('Erro ao buscar produtos:', error))
   }, [])
+
+  function handleForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const form = event.currentTarget
+    const formData = new FormData(form)
+
+    fetch('/api/produtos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    .then((response) => response.json())
+    .then((newProduto) => setProdutos([...produtos, newProduto]))
+    catch((error) => console.error('Erro ao cadastrar produto:', error))
+form .reset()
+  }
   return ( 
   <>
-  <div>Mariane</div>
+  <div>Cadastro de Produtos</div>
+  <form onSubmit={handleForm}>
+    <input type="text" name="nome" placeholder="Nome" />
+    <input type="number" name="preco" placeholder="Preço"/>
+    <input type="text" name="urlfoto" placeholder="URL da Foto" />
+    <input type="text" name="descricao" placeholder="Descrição"/>
+    <button type="submit">Cadastrar</button>
+  </form>
+  <div>Lista de Produtos</div>
   {
     produtos.map((produto) => (
       <div key={produto.id}>
