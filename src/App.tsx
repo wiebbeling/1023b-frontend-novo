e6db129c22a5440e7c7d5386d25656a44a130773
Mariane <mariane.azevedo@estudante.ifms.edu.br>
2025-09-17 11:52:08 -0300

    atualização

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+//useeffect
+import { useEffect, useState } from 'react'
+type ProdutoType = {
+  id: string,
+  nome: string,
+  preco: number,
+  urlfoto: string,
+  descricao: string
 }
 
+function App() {
+  const [produtos, setProdutos] = useState<ProdutoType[]>([])
+  useEffect(() => {
+    fetch('/api/produtos')
+      .then((response) => response.json())
+      .then((data) => setProdutos(data))
+  }, [])
+  return ( 
+  <>
+  <div>JUAN</div>
+  {
+    produtos.map((produto) => (
+      <div key={produto.id}>
+        <h2>{produto.nome}</h2>
+        <p>{produto.descricao}</p>
+        <p>R$ {produto.preco}</p>
+        <img src={produto.urlfoto} alt={produto.nome} width="200" />
+      </div>
+    ))
+  }
+  </>
+   )
+}
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import 'dotenv/config'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy:{
+      '/api': {
+        target: process.env.API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 })
